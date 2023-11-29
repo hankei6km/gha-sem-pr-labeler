69c7578856eaa2bf698b4884dbe36b14cb7bfdc1
hankei6km <gh-hankei6km@outlook.jp>
2023-11-29 05:10:17 +0000

    ci: Use yq installed on the runner

--- scripts/lint-action.sh
+++ scripts/lint-action.sh
@@ -5,13 +5,11 @@
 
 set -e
 
-YQ_BIN="${YQ_BIN:-yq}"
-
 SAVE_IFS="${IFS}"
 
 IFS=$'\0'
 while read -r -d '' s; do
     shellcheck --shell bash - <<< "${s}"
-done < <("${YQ_BIN}" .runs.steps[].run -0  < action.yml)
+done < <(yq .runs.steps[].run -0  < action.yml)
 
 IFS="${SAVE_IFS}"
